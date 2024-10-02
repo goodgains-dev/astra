ae3e95c59cf95039f36335ed165df1b4549a8d81
GoatsOnTheMoon <121653725+GoatsOnTheMoon@users.noreply.github.com>
2024-10-02 11:32:13 -0400

    Update footer.tsx
    1

--- src/components/home/navigation/footer.tsx
+++ src/components/home/navigation/footer.tsx
@@ -17,10 +17,10 @@ const Footer = () => {
                 alt="GoodGains Logo"
                 className="w-8 h-8"
               />
-                    <p className="text-muted-foreground mt-4 text-sm text-start">
+                    <p className="text-black mt-4 text-sm text-start">
                         
                     </p>
-                    <span className="mt-4 text-neutral-200 text-sm flex items-center">
+                    <span className="mt-4 text-black text-sm flex items-center">
                         <Heart className="w-3.5 h-3.5 ml-1 fill-primary text-primary" />
                     </span>
                 </div>
@@ -28,54 +28,54 @@ const Footer = () => {
                 <div className="grid-cols-2 gap-8 grid mt-16 xl:col-span-2 xl:mt-0">
                     <div className="md:grid md:grid-cols-2 md:gap-8">
                         <div className="">
-                            <h3 className="text-base font-medium text-white">
+                            <h3 className="text-base font-medium text-black">
                                 Product
                             </h3>
-                            <ul className="mt-4 text-sm text-muted-foreground">
+                            <ul className="mt-4 text-sm text-black">
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Features
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Pricing
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Testimonials
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Integration
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div className="mt-10 md:mt-0 flex flex-col">
-                            <h3 className="text-base font-medium text-white">
+                            <h3 className="text-base font-medium text-black">
                                 Integrations
                             </h3>
-                            <ul className="mt-4 text-sm text-muted-foreground">
+                            <ul className="mt-4 text-sm text-black">
                                 <li className="">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Facebook
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Instagram
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Twitter
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         LinkedIn
                                     </Link>
                                 </li>
@@ -84,44 +84,44 @@ const Footer = () => {
                     </div>
                     <div className="md:grid md:grid-cols-2 md:gap-8">
                         <div className="">
-                            <h3 className="text-base font-medium text-white">
+                            <h3 className="text-base font-medium text-black">
                                 Resources
                             </h3>
-                            <ul className="mt-4 text-sm text-muted-foreground">
+                            <ul className="mt-4 text-sm text-black">
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Blog
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Case Studies
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Support
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div className="mt-10 md:mt-0 flex flex-col">
-                            <h3 className="text-base font-medium text-white">
+                            <h3 className="text-base font-medium text-black">
                                 Company
                             </h3>
-                            <ul className="mt-4 text-sm text-muted-foreground">
+                            <ul className="mt-4 text-sm text-black">
                                 <li className="">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         About Us
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Privacy Policy
                                     </Link>
                                 </li>
                                 <li className="mt-2">
-                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                    <Link href="" className="hover:text-black transition-all duration-300">
                                         Terms & Conditions
                                     </Link>
                                 </li>
@@ -133,7 +133,7 @@ const Footer = () => {
             </div>
 
             <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full">
-                <p className="text-sm text-muted-foreground mt-8 md:mt-0">
+                <p className="text-sm text-black mt-8 md:mt-0">
                     &copy; {new Date().getFullYear()} GoodGains INC. All rights reserved.
                 </p>
             </div>
